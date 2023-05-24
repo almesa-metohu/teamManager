34a2c2ff3a5bc7fd1c8629600506b6ea7984ba5b
Almesa Metohu <almesa42@yahoo.com>
2023-05-24 18:12:09 +0200

    finished

---
+++ server/routes/player.routes.js
@@ -0,0 +1,8 @@
+const PlayerController = require('../controller/player.controller')
+
+module.exports = app => {
+    app.get('/api/players/', PlayerController.findPlayers)
+    app.post('/api/players/', PlayerController.createPlayer)
+    app.delete('/api/player/:id', PlayerController.deletePlayer)
+    app.patch('/api/players/:id/:game', PlayerController.ChangeStatusGame)
+}
